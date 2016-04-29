8d3e06a1ab8e8664d34628790aa5fa1ae5384315
MomnaSaeed <momnasaeed50@gmail.com>
2016-04-29 17:01:50 +0500

    Create OpUnitTest.java

---
+++ OpUnitTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interpreter_lab;
+
+/**
+ *
+ * @author msaeed.bscs13seecs
+ */
+public class OpUnitTest {
+    public static void main(String[] args) {
+    
+              
+                Interpreter_lab A = new Interpreter_lab();
+		A.readFromFile("D:\\MyFile.txt");
+    
+    
+}
+    
+}
