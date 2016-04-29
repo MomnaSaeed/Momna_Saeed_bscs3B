3956e4c8c8751d5935d255089bc079b39f48906e
MomnaSaeed <momnasaeed50@gmail.com>
2016-04-29 17:01:21 +0500

    Create AddUnitTest.java

---
+++ AddUnitTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interpreter_lab;
+
+/**
+ *
+ * @author msaeed.bscs13seecs
+ */
+public class AddUnitTest {
+    public static void main(String[] args) {
+    
+              
+                Interpreter_lab A = new Interpreter_lab();
+		A.readFromFile("D:\\MyFile.txt");
+    
+}
+}
